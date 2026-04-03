f201e8ca907e49a6a6dc449170870d6b8dc02dfb
Dvir Hershkovits <dvirhersh@gmail.com>
2026-04-03 13:03:21 +0300

    integrals.py
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

---
+++ math/Calculus/integrals.py
@@ -0,0 +1,13 @@
+import scipy.integrate as integrate
+
+# define function f(x) = x
+def f(x):
+    return x
+
+# Calculate integral from 0 to 2 (Area of traingle: 0.5 * base * height = 2)
+result, error = integrate.quad(f, 0, 2)
+print(f"Integral result: {result}")
+print(f"Integral error: {error}")
+
+result = integrate.quad(lambda x: x, 0, 2)
+print(result)
