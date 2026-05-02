7ac9d065815a677214781c072c9bd601a77ecc82
dvir.hershkovits <dvirhersh@gmail.com>
2026-05-02 17:49:52 +0300

    bubble sort version 1 - naive

---
+++ hebrew_python/bubble_sort.py
@@ -0,0 +1,13 @@
+# get list of items
+# buble sort the list:
+    # iterate on all indexes
+    # if adjacent items are out of order - swap them
+    # repeat n times (n is len of the list)
+
+arr = [5,7,3,7,3,7,9,2,1]
+
+for _ in range(len(arr) - 1):
+    for i in range(len(arr)-1):
+        if arr[i + 1] < arr[i]:
+            arr[i], arr[i+1] = arr[i+1], arr[i]
+print(arr)
